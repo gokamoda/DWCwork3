adf231fc4181b8605bb3296fb6a2fbb45b97f71c
takumiogikubo <104717759+takumiogikubo@users.noreply.github.com>
2022-05-06 13:42:53 +0000

    [Update]プロフィール画像の表示の試行錯誤２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,20 @@ class BooksController < ApplicationController
   end
 
   def index
+    # @user = User.find(params[:id])
+    # @books = @user.post_images
   end
 
   def show
-    @book = Book.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  # def user_params
+  #   params.require(:user).permit(:name, :profile_image)
+  # end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
+    # @books = @user.post_images
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
+
+  private
+
+  # def user_params
+  #   params.require(:user).permit(:profile_image)
+  # end
+
 end

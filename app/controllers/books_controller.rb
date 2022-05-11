0bc6bf7ef94786085f65053b9ee5e9d096210d1e
takumiogikubo <104717759+takumiogikubo@users.noreply.github.com>
2022-05-11 15:08:19 +0000

    [Update]book-indexのレイアウト編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,7 +20,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] ="You have created book successfully."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       render:index
